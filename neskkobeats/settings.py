65c687b64571113b47d6621cd26db711cd2cae62
Vadym Yankovskiy <107151785+yankkv75@users.noreply.github.com>
2022-06-10 20:52:25 +0300

    Update settings.py

--- neskkobeats/settings.py
+++ neskkobeats/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'neskkobeatsdb',
         'USER': 'postgres',
-        'PASSWORD': '321qwerty',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }
